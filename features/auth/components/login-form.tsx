335551cf49d4d219fa1b6ef1062bb60701528778
feanor77ist <a.ardas@gmail.com>
2025-01-15 12:50:20 +0300

    test fe project generated

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -26,7 +26,7 @@ export function LoginForm() {
     setError("");
 
     try {
-      const response = await fetch("https://libreconsulting.pythonanywhere.com/api/login/", {
+      const response = await fetch("http://127.0.0.1:8000/api/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }), // username ve password gönderiliyor
